506436f2e2764d1cd0bb5df3c7873d48d86ae4d5
Joao Francisco <joaojorpas@hotmail.com>
2023-02-21 16:14:12 -0300

    Adicionada aula sobre Tipos Primitivos

---
+++ 12-Módulo-JS-Básico/04-Tipos-Primitivos/src/js/index.js
@@ -0,0 +1,47 @@
+//string
+let texto = "Um texto qualquer"
+
+console.log(texto)
+
+console.log(typeof texto)
+
+//number
+let numeroInteiro = 123
+
+let numeroNegativo = -25
+
+let numeroRacional = -0.67
+
+console.log(numeroInteiro,numeroNegativo,numeroRacional)
+
+console.log(typeof numeroInteiro,typeof numeroNegativo,typeof numeroRacional)
+
+//boolean
+
+let confirmacaoVerdadeiro = true
+
+let confirmacaoFalso = false
+
+console.log(confirmacaoVerdadeiro,confirmacaoFalso)
+
+console.log(typeof confirmacaoVerdadeiro, typeof confirmacaoFalso)
+
+//null
+
+let variavelNula = null
+
+console.log(variavelNula)
+
+console.log(typeof variavelNula)
+
+//undefined
+
+let indefinida = undefined
+
+let variavelIncompleta
+
+let variavelErrada = "blabla"
+
+console.log(indefinida, variavelIncompleta)
+
+console.log(typeof indefinida,typeof variavelIncompleta,typeof variavelErrada, typeof variavelEraada)

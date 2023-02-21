4aa9731fd53d52b9dc480bb7e809c0010404fe7c
Joao Francisco <joaojorpas@hotmail.com>
2023-02-21 12:16:44 -0300

    Adicionada aula sobre Declaração de Variáveis

---
+++ 12-Módulo-JS-Básico/03-Declarando-Variáveis/src/js/index.js
@@ -0,0 +1,25 @@
+var nomePessoa = "João Francisco"
+console.log(nomePessoa)
+
+var nomePessoa = "Roberto Dias"
+console.log(nomePessoa)
+
+nomePessoa = "Ricardo Dias"
+console.log(nomePessoa)
+
+const email_pessoa = "joaojorpas@hotmail.com"
+console.log(email_pessoa)
+
+/* Erro: Não pode atribuir novo valor à variável "const"
+email_pessoa="joaojorpas@gmail.com"
+console.log(email_pessoa)*/
+
+let telefonePessoa = "40028922"
+console.log(telefonePessoa)
+
+/*Erro: Não podemos declarar novamente uma variável com let
+let telefonePessoa = "99999999"
+console.log(telefonePessoa)*/
+
+telefonePessoa = "6789998212"
+console.log(telefonePessoa)

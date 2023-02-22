58280a8912950a4a3d4645fb575f4fca069b5ba1
Joao Francisco <joaojorpas@hotmail.com>
2023-02-22 17:38:07 -0300

    Adicionada aula sobre Operadores Matemáticos

---
+++ 12-Módulo-JS-Básico/07-Operadores-Matemáticos/src/js/index.js
@@ -0,0 +1,33 @@
+const a = 13
+const b= 2
+let c = "2"
+
+console.log("Soma")
+
+console.log(a+b)
+//Retorna 15
+
+console.log("Concatenação")
+
+console.log(a+c)
+//Retorna 132
+
+console.log("Subtração")
+
+console.log(a-b)
+//Retorna 11
+
+console.log("Multiplicação")
+
+console.log(a*b)
+//Retorna 26
+
+console.log("Divisão")
+
+console.log(a/b)
+//Retorna 6.5
+
+console.log("Resto da Divisão")
+
+console.log(a%b)
+//Retorna 1

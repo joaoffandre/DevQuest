8f99157cd48b59b868fbf6a2a792703039e78ead
Joao Francisco <joaojorpas@hotmail.com>
2023-02-22 18:31:35 -0300

    Adicionadas operações ++ , --, **, -x, +x

--- 12-Módulo-JS-Básico/07-Operadores-Matemáticos/src/js/index.js
+++ 12-Módulo-JS-Básico/07-Operadores-Matemáticos/src/js/index.js
@@ -1,4 +1,4 @@
-const a = 13
+let a = 13
 const b= 2
 let c = "2"
 
@@ -31,3 +31,40 @@ console.log("Resto da Divisão")
 
 console.log(a%b)
 //Retorna 1
+
+console.log("Sucessor")
+
+console.log(a++)
+//Retorna o valor da variável a, depois soma 1 a ela.
+
+console.log(++a)
+//Soma 1 à variável a, e retorna seu novo valor.
+
+console.log("Antecessor")
+
+console.log(a--)
+//Retorna o valor da variável a, depois subtrai 1 dela.
+
+console.log(--a)
+//Subtrai uma unidade da variável a, e depois retorna seu novo valor
+
+console.log("Potenciação")
+
+console.log(a**b)
+//Retorna 169
+
+console.log("Oposto")
+
+console.log(-a)
+//Retorna -13
+
+console.log("Transforma em 'number'")
+
+console.log(+c)
+//Retorna 2
+
+console.log(+true)
+//Retorna 1
+
+console.log(+false)
+//Retorna 0

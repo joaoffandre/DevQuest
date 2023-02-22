b83fdfb71311cec5d687c3f7aeda2ad3327bec02
Joao Francisco <joaojorpas@hotmail.com>
2023-02-22 11:11:53 -0300

    Adicionada aula sobre Operadores de Comparação

---
+++ 12-Módulo-JS-Básico/05-Operadores-Lógicos/src/js/index.js
@@ -0,0 +1,79 @@
+console.log("== (igual a)")
+
+let a = 5
+const b = 5
+const c ="5"
+let d = 3
+
+//Retornam "true"
+console.log(a==b)
+
+console.log(a==c)
+
+//Retorna "false"
+console.log(a==d)
+//
+
+console.log("=== (idêntico a)")
+
+//Retorna "true"
+console.log(a===b)
+
+//Retornam "false"
+console.log(a===c)
+console.log(a===d)
+//
+
+console.log("!= (diferente de)")
+
+//Retorna "true"
+
+console.log(a!=d)
+
+//Retornam "false"
+
+console.log(a!=b)
+console.log(a!=c)
+//
+
+console.log("!== (não idêntico a)")
+
+//Retorna "true"
+
+//Retornam "false"
+
+//
+
+console.log("< (menor que)")
+
+//Retorna "true"
+
+//Retornam "false"
+
+//
+
+console.log("<= (menor ou igual a)")
+
+//Retorna "true"
+
+//Retornam "false"
+
+//
+
+console.log("> (maior que")
+
+//Retorna "true"
+
+//Retornam "false"
+
+//
+
+console.log(">= (maior ou igual a")
+
+//Retorna "true"
+
+//Retornam "false"
+
+//
+
+

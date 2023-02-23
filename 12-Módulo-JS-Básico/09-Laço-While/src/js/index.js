95a741535343030afcc03b6ae502761ea19e6e7e
Joao Francisco <joaojorpas@hotmail.com>
2023-02-23 12:52:58 -0300

    Adicionada aula sobre While e Do While

---
+++ 12-Módulo-JS-Básico/09-Laço-While/src/js/index.js
@@ -0,0 +1,12 @@
+let a= 10
+let b= 20
+
+while((a+b)/2<20){
+    console.log("Incrementar a variável a; a= " + a)
+    a++
+}
+
+do{
+    console.log("Valor da variável a: " + a)
+    ++a
+}while(a<=b)

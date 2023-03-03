61dcb7675a702a09caa65b7241927bc7e81f06a8
Joao Francisco <joaojorpas@hotmail.com>
2023-03-03 15:55:34 -0300

    Adicionada aula sobre slice e splice

---
+++ 13-Módulo-JS-Intermediário/09-slice-splice/src/js/index.js
@@ -0,0 +1,27 @@
+//Método .slice() : Retorna uma CÓPIA do array; primeiro parâmetro é o index de começo (inclusive) e segundo parâmetro é o index de final (exclusive)
+
+let jogos = ["Mario Odyssey","Zelda Botw","Metroid Dread","Pokemon Legends Arceus"]
+
+let novoJogos = jogos.slice(0,2)
+let jogos2021 = jogos.slice(-2)
+
+console.log(novoJogos)
+console.log(jogos2021)
+console.log(jogos)
+
+//Método .splice : Adiciona e Retira elementos de um Array, alterando o conteúdo do Array. Retorna os elementos retirados do Array
+
+// Adicionando elementos com .splice()
+
+console.log(jogos.splice(2,0,"Donkey Kong Tropical Freeze"))
+console.log(jogos)
+
+// Retirando elementos com .splice()
+
+console.log(jogos.splice(1,2))
+console.log(jogos)
+
+// Retirando elementos e adicionando outros com .splice()
+
+console.log(jogos.splice(1,2,"Mario Maker 2","Super Smash Bros","Mario Kart 8"))
+console.log(jogos)

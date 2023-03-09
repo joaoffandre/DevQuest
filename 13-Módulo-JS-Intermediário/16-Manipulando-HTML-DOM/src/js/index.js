e3adf145265a08ee29a7448ec3ffc127a5d0fe4e
Joao Francisco <joaojorpas@hotmail.com>
2023-03-08 21:10:22 -0300

    Aula: Manipulando Elementos HTML pelo DOM

---
+++ 13-Módulo-JS-Intermediário/16-Manipulando-HTML-DOM/src/js/index.js
@@ -0,0 +1,14 @@
+//Ao Digitar um comentário e clicar no "button", a função adicionarComentario() é chamada
+
+function adicionarComentario(){
+    //colocando o input digitado em uma variável
+    let inputComentario = document.getElementsByName("novo-comentario")
+    //"pegando" o valor digitado no input através da propriedade "value"
+    let comentario = inputComentario[0].value
+
+    //Armazenando o elemento HTML ao qual queremos adicionar o comentário em uma variável
+    let novosComentarios = document.getElementById("novos-comentarios")
+
+    //Acessando o conteúdo do elemento através da propriedade .innerHTML e adicionando conteúdo ao elemento
+    novosComentarios.innerHTML += `<p>Novo comentário: ${comentario}</p>`
+}

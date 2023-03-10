db406223e73b61a37504fb27df7cbd7bd4c59781
Joao Francisco <joaojorpas@hotmail.com>
2023-03-10 19:50:16 -0300

    Aula: Identificando Erros no JS

---
+++ 13-Módulo-JS-Intermediário/19-Resolvendo-Erros-JS/src/js/index.js
@@ -0,0 +1,12 @@
+// let campeaoGaucho = "Grêmio"
+
+// console.log(campeaoGauch)
+
+
+let inputTelefone = document.getElementById("telefone")
+
+inputTelefone.disabled = true
+
+function habilitarTelefone(){
+    inputTelefone.disabled = false;
+}

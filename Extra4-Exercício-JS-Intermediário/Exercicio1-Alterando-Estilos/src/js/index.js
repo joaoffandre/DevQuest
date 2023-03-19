bcf3292c16fc8de59f5a23200a5d68d25ca374c5
Joao Francisco <joaojorpas@hotmail.com>
2023-03-18 22:12:46 -0300

    Exercício 1- Adicionando Estilos - Concluído

---
+++ Extra4-Exercício-JS-Intermediário/Exercicio1-Alterando-Estilos/src/js/index.js
@@ -0,0 +1,17 @@
+let caixa1 = document.getElementsByClassName("caixa")[0]
+let botao = document.getElementById("botao")
+
+function trocarCor(){
+    caixa1.style.backgroundColor="yellow"
+}
+
+botao.addEventListener("click",trocarCor)
+
+
+/* Para mudar a cor de fundo podemos adicionar e remover as classes que o elemento tem. Ex:
+
+function trocarCor() {
+    quadradoAzul.classList.remove("azul");
+    quadradoAzul.classList.add("amarelo");
+  }
+  */
